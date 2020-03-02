632dcd9e0446ddf4cd85840964f411330dd2ddce
Nicholas Duffy <duffn@outlook.com>
2020-03-01 19:57:45 -0700

    Goofy pace try

--- gatsby-config.js
+++ gatsby-config.js
@@ -159,12 +159,12 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-nprogress`,
-      options: {
-        color: `tomato`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-nprogress`,
+    //   options: {
+    //     color: `tomato`,
+    //   },
+    // },
   ],
   siteMetadata: {
     title: `The Outer Rim`,
--- src/components/shared/TopLayout.js
+++ src/components/shared/TopLayout.js
@@ -26,6 +26,17 @@ export default function TopLayout(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"
+          integrity="sha256-EPrkNjGEmCWyazb3A/Epj+W7Qm2pB9vnfXw+X6LImPM="
+          crossorigin="anonymous"
+        ></script>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/red/pace-theme-minimal.min.css"
+          integrity="sha256-wodX26KB8CNuKfL579cYVli60HW6R9GlG6Zn7cGGZO0="
+          crossorigin="anonymous"
+        />
       </Helmet>
       <ThemeProvider theme={theme}>
         <CssBaseline />
